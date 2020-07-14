f22a9cc651a7e0194aed9b221aa336b5aaaaf1b5
Sharan Kumar <sharanblr@yahoo.co.in>
2020-07-14 12:21:24 +0530

    Initial hiddenSheet component

---
+++ src/components/HiddenSheet/HiddenSheet.stories.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import HiddenSheet from './HiddenSheet';
+
+export default {
+  title: 'Hidden Sheet',
+  component: HiddenSheet,
+  decorators: [storyFn => <div style={{}}>{storyFn()}</div>]
+};
+
+export const HiddenSheetFromRight = () => {
+  const [isOpen, setOpen] = useState(false);
+  const onToggle = () => {
+    setOpen(!isOpen);
+  };
+  return (
+    <div>
+      <button onClick={onToggle}>Toggle State</button>
+      <HiddenSheet
+        isOpen={isOpen}
+        onToggle={onToggle}
+        originDirection="right"
+        closeIcon
+      >
+        <div
+          style={{
+            backgroundColor: 'goldenrod',
+            height: '200px',
+            width: '100px'
+          }}
+        >
+          This is the content!
+        </div>
+      </HiddenSheet>
+    </div>
+  );
+};
+
+export const HiddenSheetFromBottom = () => {
+  const [isOpen, setOpen] = useState(false);
+  const onToggle = () => {
+    setOpen(!isOpen);
+  };
+  return (
+    <div>
+      <button onClick={onToggle}>Toggle State</button>
+      <HiddenSheet
+        isOpen={isOpen}
+        onToggle={onToggle}
+        originDirection="bottom"
+        closeIcon
+      >
+        <div
+          style={{
+            backgroundColor: 'goldenrod',
+            height: '200px',
+            width: '100px'
+          }}
+        >
+          This is the content!
+        </div>
+      </HiddenSheet>
+    </div>
+  );
+};
